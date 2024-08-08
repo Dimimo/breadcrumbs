2a032fa082bd5d63154f433518b6131de3b3d819
Dimitri Mostrey <dmostrey@yahoo.com>
2024-08-08 11:53:13 +0200

    Tailwind improved

--- views/tailwind.blade.php
+++ views/tailwind.blade.php
@@ -1,23 +1,22 @@
 @if (count($breadcrumbs))
 
-    <ul class="flex items-center justify-center font-[sans-serif] space-x-4 mt-4">
+    <ul class="flex items-center justify-center font-[sans-serif] mt-4">
         @foreach ($breadcrumbs as $breadcrumb)
             @if ($breadcrumb->url && !$loop->last)
 
-                <li class="text-gray-500 text-base cursor-pointer">
-                    Home
+                <li class="text-gray-500 text-lg cursor-pointer">
+                    <a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
                 </li>
                 <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-400 w-3.5 -rotate-90" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd"
-                              d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z"
-                              clip-rule="evenodd" data-original="#000000"></path>
+                    <svg class="fill-gray-400 w-3 h-3 mx-3 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path fill-rule="evenodd" clip-rule="evenodd" data-original="#000000"
+                            d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
                     </svg>
                 </li>
             @else
 
-                <li class="text-gray-500 text-base cursor-pointer">
-                    Profile
+                <li class="text-gray-500 text-lg text-">
+                    {{ $breadcrumb->title }}
                 </li>
             @endif
         @endforeach
