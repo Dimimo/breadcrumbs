8793a21dd4a68b909ae5809fa8cb847b69adc0b9
Dimitri Mostrey <dmostrey@yahoo.com>
2024-08-16 11:26:31 +0200

    Tailwind with caret even without link

--- views/tailwind.blade.php
+++ views/tailwind.blade.php
@@ -10,7 +10,18 @@
                 <li>
                     <svg class="fill-gray-400 w-3 h-3 mx-3 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                         <path fill-rule="evenodd" clip-rule="evenodd" data-original="#000000"
-                            d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                              d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                    </svg>
+                </li>
+            @elseif (!$loop->last)
+
+                <li class="text-gray-500 text-lg">
+                    {{ $breadcrumb->title }}
+                </li>
+                <li>
+                    <svg class="fill-gray-400 w-3 h-3 mx-3 mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path fill-rule="evenodd" clip-rule="evenodd" data-original="#000000"
+                              d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
                     </svg>
                 </li>
             @else
